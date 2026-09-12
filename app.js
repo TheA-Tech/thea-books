10522035f3d6f49f3a998a170890d786da9aafb2
The A.Tech <164446501+TheA-Tech@users.noreply.github.com>
2026-09-12 19:10:28 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const SUPABASE_URL = "https://iruedykdeuyejrrcsaed.supabase.co";
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlydWVkeWtkZXV5ZWpycmNzYWVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNzIxODIsImV4cCI6MjEwNDc0ODE4Mn0.VKuAv1goH4hebSobkkfddeUpwfM3t-FtG2VvMPOk6HUconst pageTitle = document.getElementById("pageTitle");
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlydWVkeWtkZXV5ZWpycmNzYWVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNzIxODIsImV4cCI6MjEwNDc0ODE4Mn0.VKuAv1goH4hebSobkkfddeUpwfM3t-FtG2VvMPOk6HU";
+
+const pageTitle = document.getElementById("pageTitle");
 const content = document.getElementById("content");
 
 let theaSupabase = null;
